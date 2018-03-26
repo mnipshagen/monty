c782b48d6fd70532f37caf24e4f301cb94909e3b
a-hain <ahain@uni-osnabrueck.de>
2018-03-27 00:42:15 +0200

    Added Ex. 1 solutions

---
+++ 2018/01/nicholas.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.shape('turtle')
+
+# left edge
+turtle.left(90)
+turtle.forward(100)
+
+# roof
+turtle.right(45)
+turtle.forward(70.71)
+turtle.right(90)
+turtle.forward(70.71)
+turtle.right(135)
+turtle.forward(100)
+
+# cross and right edge
+turtle.left(135)
+turtle.forward(141.42)
+turtle.right(45) # !
+turtle.backward(100)
+turtle.right(45)
+turtle.forward(141.42)
+
+# bottom edge
+turtle.left(135)
+turtle.forward(100)
+
+# keep the window open (not needed for the homework)
+turtle.done()
