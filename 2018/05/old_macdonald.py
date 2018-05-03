60e704146dc75b5e8a73963b57004259fb2b4177
Moritz Nipshagen <mnipshagen@uos.de>
2018-05-03 15:06:39 +0200

    homework finished

--- 2018/05/old_macdonald.py
+++ 2018/05/old_macdonald.py
@@ -1,21 +1,24 @@
 import re
 
+# this is just to read in the song text.
+# stay tuned for next week when we talk about file in- & output
 def read_verse(filename):
+    """This reads in a file and returns a its contents."""
     with open(filename) as f:
         verse = f.read()
     f.closed
 
     return verse
 
+# implement this
 def delete_meow(verse):
     return verse
 
+# impement this as well
 def sing_song(verse):
     print(verse)
 
-
+# and here we go!
 verse = read_verse('old_macdonald.txt')
-
 verse = delete_meow(verse)
-
 sing_song(verse)
