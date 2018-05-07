d79309b936ad040b917fb4eec61d66b2e65cfe84
Moritz Nipshagen <mnipshagen@uos.de>
2018-05-07 18:47:04 +0200

    week 06

---
+++ 2018/06/task_2.py
@@ -0,0 +1,46 @@
+my_lst = [
+    {
+        "key1" : "Potatosalad",
+        "key2" : 42,
+        "key3" : ["Potato", "Oil", "Vinegar", "Onion"]
+    },
+    {
+        "key1" : "Sandwich",
+        "key2" : 12,
+        "key4" : ["Cookie", "Softdrink"] 
+    },
+    {
+        "key1" : "Pizza",
+        "key2" : 35,
+        "key3" : ["Dough", "Tomato", "Oregano", "Cheese"],
+        "keykey" : ["Bellpepper", "Sweetcorn", "Brokkoli"]
+    },
+    {
+        "keya" : "Food",
+        "key2" : 1,
+        "keyb" : ["Generic_Ingredient", "Needed_Ingredient", "Special_Ingredient"]
+    },
+]
+my_lst_sorted = [
+    {
+        'keya': 'Food',
+        'key2': 1,
+        'keyb': ['Generic_Ingredient', 'Needed_Ingredient','Special_Ingredient']
+    },
+    {
+        'key1': 'Sandwich',
+        'key2': 12,
+        'key4': ['Cookie', 'Softdrink']
+    },
+    {
+        'key1': 'Pizza',
+        'key2': 35,
+        'key3': ['Dough', 'Tomato', 'Oregano', 'Cheese'],
+        'keykey': ['Bellpepper', 'Sweetcorn', 'Brokkoli']
+    },
+    {
+        'key1': 'Potatosalad',
+        'key2': 42,
+        'key3': ['Potato', 'Oil', 'Vinegar', 'Onion']
+    }
+]
