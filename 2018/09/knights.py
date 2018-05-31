e37954a72c5026108d314c3b83b98461dd0891a0
Moritz Nipshagen <moritz.nipshagen@protonmail.com>
2018-06-01 00:17:05 +0200

    Ex09

--- 2018/09/knights.py
+++ 2018/09/knights.py
@@ -37,7 +37,7 @@ class Knight:
 
     def light_attack(self):
         crit = random.randint(0,99) < self.luck*2
-        return int((1 + 0.15 * self.strength) * self.crit_modifier ** (int(crit))), crit
+        return 1 + int((.5 + 0.2 * self.strength) * self.crit_modifier ** (int(crit))), crit
 
     def heavy_hit(self):
         crit = random.randint(0, 99) < self.luck
@@ -57,11 +57,47 @@ class BlackKnight(Knight):
 
     def __init__(self, name, vitality, strength, luck):
         super().__init__(name, vitality, strength, luck)
-        self.available_actions += [self.execute]
+        self.luck = 0
 
-    def execute(self):
-        return 0, False
+    def light_attack(self):
+        dmg, crit = super().light_attack()
+        dmg *= 1.4
+        return int(dmg), crit
+
+    def heavy_hit(self):
+        dmg, crit = super().heavy_hit()
+        dmg *= 1.4
+        return int(dmg), crit
 
     def description():
-        return ("The black knight is a dangerous foe, who deals 1.8 times "
+        return ("The black knight is a dangerous foe, who deals 1.4 times "
             "damage, but cannot crit")
+
+
+class Priest(Knight):
+
+    def __init__(self, name, vitality, strength, luck):
+        super().__init__(name, vitality, strength, luck)
+        self.vitality = int(1.5 * MAX_ATTRIBUTE_POINTS) + int(1.1 * vitality)
+        self._health = self.vitality
+        self._available_heals = 5
+        del self.available_actions[self.available_actions.index(self.heavy_hit)]
+        self.available_actions += [self.heal]
+
+    def heal(self):
+        if self._available_heals > 0:
+            self._available_heals -= 1
+            crit = random.randint(0, 99) < self.luck
+            self._health += int((self.vitality - self._health)/2 + .5) * int((self.crit_modifier/2+1)) ** (int(crit))
+            return 0, crit
+        else:
+             return self.light_attack()
+
+    def description():
+        return (
+            "Priests scale worse with vitality and cannot use heavy hit, but they can heal "
+            "themselves. Healing is more effective the less health the priest has and can "
+            "critically strike. Crits  overheal priests going over their max health. A "
+            "priest can heal up to 5 times per battle. When a priest cannot heal anymore "
+            "their heals turn into light attacks."
+        )
